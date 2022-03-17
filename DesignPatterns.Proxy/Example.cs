ae539d70207d053afecc41a3c424a1ef2e337a90
Fati Iseni <fati.iseni@pozitrongroup.com>
2022-03-17 10:39:46 +0100

    Added Proxy sample.

---
+++ DesignPatterns.Proxy/Example.cs
@@ -0,0 +1,61 @@
+﻿namespace DesignPatterns.Proxy.Classic;
+
+public class Example
+{
+    public void Run()
+    {
+        var proxy = new MathProxy();
+
+        // Do the math
+        Console.WriteLine("4 + 2 = " + proxy.Add(4, 2));
+        Console.WriteLine("4 - 2 = " + proxy.Sub(4, 2));
+        Console.WriteLine("4 * 2 = " + proxy.Mul(4, 2));
+        Console.WriteLine("4 / 2 = " + proxy.Div(4, 2));
+    }
+}
+
+public interface IMath
+{
+    double Add(double x, double y);
+    double Sub(double x, double y);
+    double Mul(double x, double y);
+    double Div(double x, double y);
+}
+
+public class Math : IMath
+{
+    public double Add(double x, double y) { return x + y; }
+    public double Sub(double x, double y) { return x - y; }
+    public double Mul(double x, double y) { return x * y; }
+    public double Div(double x, double y) { return x / y; }
+}
+
+public class Math2 : IMath
+{
+    public double Add(double x, double y) { return x + y + 1; }
+    public double Sub(double x, double y) { return x - y; }
+    public double Mul(double x, double y) { return x * y; }
+    public double Div(double x, double y) { return x / y; }
+}
+
+public class MathProxy : IMath
+{
+    private readonly Math math = new();
+
+    public double Add(double x, double y)
+    {
+        return math.Add(x, y) + 1;
+    }
+    public double Sub(double x, double y)
+    {
+        return math.Sub(x, y);
+    }
+    public double Mul(double x, double y)
+    {
+        return math.Mul(x, y);
+    }
+    public double Div(double x, double y)
+    {
+        return math.Div(x, y);
+    }
+}
---
+++ DesignPatterns.Proxy/Program.cs
@@ -0,0 +1,3 @@
+﻿using DesignPatterns.Proxy.Classic;
+
+new Example().Run();
